6059b22a7bba8dc92d863094deeb4aeb9cfe55df
JedKap <jed@halliday.finance>
2026-04-30 12:05:09 -0400

    feat:  logos and improve SVG consistency

---
+++ tokens/index.ts
@@ -0,0 +1,10 @@
+/// <reference path="../assets.d.ts" />
+
+export { default as ape } from './ape.svg';
+export { default as arbitrum } from './arbitrum.png';
+export { default as avalanche } from './avalanche.png';
+export { default as bsc } from './bsc.png';
+export { default as dogecoin } from './dogecoin.svg';
+export { default as polygon } from './polygon.png';
+export { default as story } from './story.png';
+export { default as ton } from './ton.svg';
